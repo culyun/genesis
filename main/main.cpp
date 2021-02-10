01719efd71e5471cbc00c7127c873cfa9fa8474c
Steve Simpson <ssimpson64@gmail.com>
2021-02-10 20:34:41 +1300

    Added main/main.cpp to quickly try out code during development

---
+++ main/main.cpp
@@ -0,0 +1,33 @@
+
+
+#include <iostream>
+#include <string>
+#include <type_traits>
+#include <concepts>
+
+#include "../machine/endian.hpp"
+
+#include "../machine/fixed-point.hpp"
+using namespace machine;
+
+int main() 
+{
+  std::cout << std::string(std::is_same_v<int32_t, decltype(FastestInteger<32, true>())> ? "int32_t" : "not int32_t") << std::endl;
+  std::cout << std::string(std::is_same_v<int64_t, decltype(FastestInteger<32, true>())> ? "int64_t" : "not int64_t") << std::endl;
+
+  FixedPoint<63, 3> steve(9);
+  FixedPoint<13, 5> nenny(5);
+
+  auto george = steve * nenny;
+
+  std::cout << "george.bits = " << george.Bits << ", .power " << george.Power << ", .value " << george.getData() << std::endl;
+
+
+    uint32le_t fred = 1;
+  int32le_t jo = 3;
+  uint32be_t sum = fred << (jo);
+
+  std::cout << "sum is " << sum << std::endl;
+
+  return 0;
+}
